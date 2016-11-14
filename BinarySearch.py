049fc8c82ef117988ba6d9ea53d95d780dc21ddb
GathuBoswell <gathudev@gmail.com>
2016-11-14 23:45:10 +0300

    added the serach function

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,8 +1,12 @@
 class BinarySearch(list):
 
     def __init__(self, length, step):
+        super().__init__()
         self.__length = length
         self._step = step
+        self._count = 0
+        self._upper = length
+        self._lower = 0
 
         for x in range(self.__length):
             list.append(self, self._step)
@@ -12,4 +16,18 @@ class BinarySearch(list):
     def length(self):
         return self.__length
 
+    def search(self, search_item):
+        cursor = (self._upper + self._lower) // 2
+        if self[cursor] == search_item:
+            return {'count': self._count, 'index': cursor}
+        elif self[cursor] > search_item:
+            self._upper = cursor - 1
+            self._count += 1
+            return self.search(search_item)
+        elif self[cursor] < search_item:
+            self._lower = cursor + 1
+            self._count += 1
+            return self.search(search_item)
+
 print(BinarySearch(10, 1))
+print(BinarySearch(10, 1).search(2))
